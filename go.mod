5a4b0a591fecc69479c0be44d6ff86974be5cc05
tongzi <tongzi@ibantang.com>
2021-12-27 15:25:47 +0800

    INIT

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/go-mysql-org/go-mysql v1.4.0
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/satori/go.uuid v1.2.0
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/yaml.v2 v2.2.2
 )
--- src/main.go
+++ src/main.go
@@ -2,20 +2,48 @@ package main
 
 import (
 	"flag"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	log "github.com/sirupsen/logrus"
 	"go-mysql2es/src/config"
 	"go-mysql2es/src/core"
+	"go-mysql2es/src/utils"
+	"time"
 )
 
 type Context struct {
 	conf *config.Conf
 }
 
+//切割日志和清理过期日志
+func configLocalFilesystemLogger(filePath string) {
+	writer, err := rotatelogs.New(
+		filePath+".%Y%m%d",
+		rotatelogs.WithLinkName(filePath),         // 生成软链，指向最新日志文件
+		rotatelogs.WithRotationTime(time.Hour*12), // 日志切割时间间隔
+		rotatelogs.WithRotationCount(3),
+	)
+	if err != nil {
+		log.Panicf("Init log failed, err:", err)
+	}
+	log.SetOutput(writer)
+	log.SetLevel(log.InfoLevel)
+}
+
 func main() {
 	var confPath = flag.String("conf", "", "配置文件路径")
+	var logPath = flag.String("log", "", "日志路径")
 	flag.Parse()
 	if *confPath == "" {
 		log.Panic("-conf 配置文件路径")
+	}
+	if *logPath == "" {
+		configLocalFilesystemLogger("./mysql2es.log")
+	} else {
+		if !utils.IsDir(*logPath) {
+			log.Panicf("日志文件路径 %v 检测失败", *logPath)
+		} else {
+			configLocalFilesystemLogger(*logPath + "/mysql2es.log")
+		}
 	}
 	conf := config.Load(confPath)
 	syncer := core.New(conf)
